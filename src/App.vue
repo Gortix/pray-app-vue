3ac83af95ca4c393a2b7d161b290e235c5eedd4f
Paweł Piechowiak <pawelpiecho1992@o2.pl>
2022-12-13 17:17:46 +0100

    add selection

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,16 @@
       <q-header elevated class="bg-primary text-white" height-hint="98">
         <PageHeader />
       </q-header>
-
       <q-page-container class="row wrap q-mt-sm q-pa-xs" v-if="data[0]">
         <PrayBox
+          v-touch-hold.mouse="(details: Details)=>touchHoldHandler(details, rec.id as string)"
           v-for="rec in store.data"
           :key="rec.id"
           v-bind="rec"
           :owner="rec.owner.name"
           @remove-doc="() => store.removePray(rec.id as string)"
           :my-pray="rec.owner.id == auth.profile.id"
+          :selected="isSelected(rec.id as string)"
         />
       </q-page-container>
       <AddPrayCompoment />
@@ -25,7 +26,7 @@
 import PrayBox from "./components/PrayBox.vue";
 import AddPrayCompoment from "./components/AddPrayCompoment.vue";
 import { useStore } from "@/store/index";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import PageHeader from "./components/PageHeader.vue";
 import { useAuth } from "./store/auth";
 import LoginPage from "./components/LoginPage.vue";
@@ -35,6 +36,39 @@ const auth = useAuth();
 const store = useStore();
 
 const data = computed(() => store.getSortedData);
+
+const selectedList = ref<string[]>([]);
+
+const isSelected = (recID: string) => {
+  return selectedList.value.findIndex((el) => el == recID) >= 0;
+};
+
+interface Details {
+  evt: Event;
+  touch: boolean;
+  mouse: boolean;
+  position: { top: number; left: number };
+  duration: number;
+}
+const touchHoldHandler = (details: Details, recID: string) => {
+  console.log(details.evt);
+  const indexOfSelectedPray = selectedList.value.findIndex(
+    (element) => {
+      console.log(element);
+      
+      return element == recID}
+  );
+  console.log(indexOfSelectedPray);
+
+  if (indexOfSelectedPray >= 0) {
+    selectedList.value.splice(indexOfSelectedPray, 1);
+    return;
+  }
+  selectedList.value.push(recID);
+  console.log(selectedList.value);
+
+  // checkedPrays.value[recID] = !checkedPrays.value[recID];
+};
 
 // navigator.clipboard.writeText('Text to get copied')
 mainAuthObject.onAuthStateChanged(async (user) => {
@@ -50,7 +84,7 @@ mainAuthObject.onAuthStateChanged(async (user) => {
 });
 </script>
 <style lang="scss">
-*{
+* {
   box-sizing: border-box;
 }
 
