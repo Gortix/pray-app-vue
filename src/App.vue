5e397e681bb2696ef9636cbb083b5a585e6db729
Paweł Piechowiak <pawelpiecho1992@o2.pl>
2022-12-14 16:39:45 +0100

    add control apnnel and animation

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
   <LoginPage v-if="!auth.loggedIn" @login="auth.authorize" />
   <Suspense v-else>
     <q-layout view="hHh lpR fFf">
-      <q-header elevated class="bg-primary text-white" height-hint="98">
-        <PageHeader />
+      <q-header class="bg-white text-white" height-hint="98">
+        <PageHeader class="bg-primary text-white" style="z-index: 100" />
+        <Transition>
+          <ControlPanel
+            class="absolute absolute-top full-height"
+            v-if="renderPanel"
+            style="z-index: 100000"
+          />
+        </Transition>
       </q-header>
 
-      <q-page-container class="row wrap q-mt-sm q-pa-xs" v-if="data[0]">
+      <q-page-container
+        class="row wrap q-mt-sm q-pa-xs custom-gap"
+        v-if="data[0]"
+      >
         <PrayBox
           v-for="rec in store.data"
           :key="rec.id"
@@ -16,6 +26,7 @@
           :my-pray="rec.owner.id == auth.profile.id"
         />
       </q-page-container>
+      <button @click="() => (renderPanel = !renderPanel)">hide</button>
       <AddPrayCompoment />
     </q-layout>
   </Suspense>
@@ -25,14 +36,17 @@
 import PrayBox from "./components/PrayBox.vue";
 import AddPrayCompoment from "./components/AddPrayCompoment.vue";
 import { useStore } from "@/store/index";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import PageHeader from "./components/PageHeader.vue";
 import { useAuth } from "./store/auth";
 import LoginPage from "./components/LoginPage.vue";
 import { auth as mainAuthObject } from "./@firebase/index";
+import ControlPanel from "./components/ControlPanel.vue";
 
 const auth = useAuth();
 const store = useStore();
+
+const renderPanel = ref(true);
 
 const data = computed(() => store.getSortedData);
 
@@ -50,14 +64,26 @@ mainAuthObject.onAuthStateChanged(async (user) => {
 });
 </script>
 <style lang="scss">
-*{
+* {
   box-sizing: border-box;
 }
 
-.row {
+.custom-gap {
   gap: 0.6rem;
   @media (width > $tablet) {
     gap: 1rem;
   }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s linear;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: scaleX(0) translateX(-50%);
+  transform-origin: left top;
+  // perspective: 1000;
 }
 </style>
---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="panel">
+    <q-checkbox class="full-height" size="xs" v-model="color" color="teal" label="Wszystkie" />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const color = ref(false);
+</script>
+<style lang="scss">
+.panel {
+  width: 100%;
+  background-color: $blue-grey-1;
+  color: black;
+}
+</style>
