683bacf50cac18cfa883000a167a27e1b27f201a
Gortix <pawelpiecho1992@o2.pl>
2023-01-14 13:40:06 +0100

    Developement (#28)
    * Add remove option
    
    * Authorization (#1)
    
    * Add suthorization store
    
    * auth fix
    
    * add loggin by button, condition on no logged in
    
    * add signout button and remember login user
    
    * remover index.html
    
    * refactoring code formating
    
    * fix:add dand delete pray in UI and praybox size (#2)
    
    * fix:add dand delete pray in UI and praybox size
    
    * remove autAndRefresh method
    
    * Feat/profile menu (#3)
    
    * add form to change name
    
    * fix: searching profile by user. refactoring code
    
    * Feat/sorting (#7)
    
    * add: sorting; new pray tag; highlighted own pray
    
    * fix add pray owner list; change new ison to badge
    
    * add control apnnel and animation (#8)
    
    * add control apnnel and animation
    
    * remove test button
    
    * remove firebase file
    
    * Feat/select records (#9)
    
    * add selection
    
    * fix fisplaying border on select; refactoring code
    
    * Feat/copy selected (#11)
    
    * add copy option
    
    * fix: selection records; refactoring copying data
    
    * Feat/filters (#12)
    
    * add filter menu and functions to filters
    
    * colors for filter name
    
    * move filter control to store
    
    * fix helper
    
    * feat: add datefield to form (#14)
    
    * feat: add datefield to form
    
    * add notify
    
    * Feat/create user (#15)
    
    * feat: add datefield to form
    
    * add notify
    
    * feat: autocreate user profile
    
    * feat: autocreate user profile
    
    * feat: add new profile (#16)
    
    * fix: close button on show add new user
    
    * feat: add new profile (#17)
    
    * add animations
    
    * add animations (#18)
    
    * add error on submit
    
    * addd view to PrayBox
    
    * move app components and logic to separated modules
    
    * pray view mode
    
    * Remove edit mode from form
    
    * Feat/edit prays (#20)
    
    * Add edit mode
    
    * add admin mode to the PrayBox
    
    * replace langht to 0
    
    * some mobile changes
    
    * Feat/routing (#24)
    
    * add routes
    
    * Feat/user management (#23)
    
    * add User  management route
    
    * update user activities
    
    * Ref/change database (#21)
    
    * change databases
    
    * some small changes
    
    * add edit user table
    
    * remove console.log
    
    * remove interface from admin store
    
    * remove space
    
    * remove comments from index store
    
    * remove console log from index store
    
    * add navigation button to the user section
    
    * add some animations
    
    * remove logo
    
    * login function exec
    
    * Feat/owner filter (#25)
    
    * Add filter store
    
    * move praybox to separate folder; move getProfileOptions to store; create new filters; set filters at bottom
    
    * move filters panel to separate component;
    add badges
    
    * remove u
    
    * Typecript filter fix
    
    * profile selected component; change filters selection (#26)
    
    * fix suggestion after adding new profile
    
    * multipages
    
    * move firebase config to separate folder

---
+++ src/@firebase/config.ts
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPadQ_aiYVLLjONahtyZWw9V96pFfnApI",
+
+  authDomain: "modlitwy-dzialdowo.firebaseapp.com",
+
+  databaseURL:
+    "https://modlitwy-dzialdowo-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "modlitwy-dzialdowo",
+
+  storageBucket: "modlitwy-dzialdowo.appspot.com",
+
+  messagingSenderId: "1008530903917",
+
+  appId: "1:1008530903917:web:0b658a162cbfd303a44064",
+
+  measurementId: "G-96MYD426QP",
+};
