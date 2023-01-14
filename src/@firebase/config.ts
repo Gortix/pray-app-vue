61b3934a4860e2f506a42d1873b5af2170486b54
Paweł Piechowiak <pawelpiecho1992@o2.pl>
2023-01-14 13:55:08 +0100

    config

--- src/@firebase/config.ts
+++ src/@firebase/config.ts
@@ -1,18 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBPadQ_aiYVLLjONahtyZWw9V96pFfnApI",
-
-  authDomain: "modlitwy-dzialdowo.firebaseapp.com",
-
-  databaseURL:
-    "https://modlitwy-dzialdowo-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "modlitwy-dzialdowo",
-
-  storageBucket: "modlitwy-dzialdowo.appspot.com",
-
-  messagingSenderId: "1008530903917",
-
-  appId: "1:1008530903917:web:0b658a162cbfd303a44064",
-
-  measurementId: "G-96MYD426QP",
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAV9MZw63cJubTUMj8C6bm5G5xTn4s847c",
+  
+    authDomain: "pray-app-dzialodow.firebaseapp.com",
+  
+    databaseURL:
+      "https://pray-app-dzialodow-default-rtdb.europe-west1.firebasedatabase.app/",
+  
+    projectId: "pray-app-dzialodow",
+  
+    storageBucket: "pray-app-dzialodow.appspot.com",
+  
+    messagingSenderId: "817782987044",
+  
+    appId: "1:817782987044:web:1adeb2e41ac9e4246a7bb1",
+  };
+  
