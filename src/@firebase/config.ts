202150019426f660d17365ef9390a8ee425ab82e
Paweł Piechowiak <pawelpiecho1992@o2.pl>
2023-01-17 22:16:48 +0100

    add deploying od production and development

--- src/@firebase/config.ts
+++ src/@firebase/config.ts
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const productionFirebaseConfig = {
   apiKey: "AIzaSyBPadQ_aiYVLLjONahtyZWw9V96pFfnApI",
 
   authDomain: "modlitwy-dzialdowo.firebaseapp.com",
@@ -16,3 +16,25 @@ export const firebaseConfig = {
 
   measurementId: "G-96MYD426QP",
 };
+
+const developmentFirebaseConfig = {
+  apiKey: "AIzaSyAV9MZw63cJubTUMj8C6bm5G5xTn4s847c",
+
+  authDomain: "pray-app-dzialodow.firebaseapp.com",
+
+  databaseURL:
+    "https://pray-app-dzialodow-default-rtdb.europe-west1.firebasedatabase.app/",
+
+  projectId: "pray-app-dzialodow",
+
+  storageBucket: "pray-app-dzialodow.appspot.com",
+
+  messagingSenderId: "817782987044",
+
+  appId: "1:817782987044:web:1adeb2e41ac9e4246a7bb1",
+};
+
+// eslint-disable-next-line
+export default !!process.env.VUE_APP_FIREBASE_PROD
+  ? productionFirebaseConfig
+  : developmentFirebaseConfig;
